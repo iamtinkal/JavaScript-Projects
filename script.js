294b55f3dda2562e22abd80f988bf21519537392
iamtinkal <116683248+iamtinkal@users.noreply.github.com>
2024-08-25 21:50:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll('.button')
+// console.log(buttons[0].innerText);
+const body = document.querySelector('body')
+
+
+document.addEventListener('click', function(e){
+    if(e.target.id == 'Red'){
+        body.style.backgroundColor = e.target.id
+    }
+    if(e.target.id == 'Blue'){
+        body.style.backgroundColor = e.target.id
+    }
+    if(e.target.id == 'Green'){
+        body.style.backgroundColor = e.target.id
+    }
+    if(e.target.id == 'Yellow'){
+        body.style.backgroundColor = e.target.id
+    }
+    if(e.target.id == 'Reset'){
+        body.style.backgroundColor = 'white'    }
+
+})
