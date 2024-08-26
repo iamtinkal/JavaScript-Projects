be96084efb747f0f266f3184f11ba672675b4c3e
iamtinkal <116683248+iamtinkal@users.noreply.github.com>
2024-08-26 11:16:54 +0000

    project 2

--- Project 2 _ BMI Calculator/script.js
+++ Project 2 _ BMI Calculator/script.js
@@ -1,21 +1,28 @@
 const form = document.querySelector('form')
+const result  = document.querySelector('#results')
 
-document.querySelector('#height').style.color = 'green'
-
-console.log("script running")
-// form.addEventListener('submit', function(e){
-//     e.preventDefault();
-//     //select height
-//     const height = parseInt(document.querySelector('#height').value)
-//     const weight = parseInt(document.querySelector('#weight').value)
-//     const result = document.querySelector('#results').innerHTML
-//     console.log(result)
-//     console.log(e.target);
+form.addEventListener('submit', function(e){
+    e.preventDefault()
     
-//     // //Checks
-//     // if(height<=0 || isNaN(height)|| height === ''){
-//     //     result = "Please enter Valid Number"
-//     // } else if(weight<=0 || isNaN(weight)|| weight === ''){
-//     //     result = "Please enter Valid Number"
-//     // }
-// })
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    
+
+    //check if wrong number enter by user
+    if(height<=0|| height === '' || isNaN(height)){
+        result.innerHTML = "Please Enter valid height"
+    }
+    else if(weight<=0|| weight === '' || isNaN(weight)){
+        result.innerHTML = "Please Enter valid weight"
+    }
+    else{
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        //show the result
+        results.innerHTML = `<span>BMI is: ${bmi}</span>`;
+    }
+})
+
+
+document.querySelector('#reset').addEventListener('click', function(e){
+    result.innerHTML = ''
+})
