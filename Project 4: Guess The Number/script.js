90310533da3d72abf6470c7736c852aa9ac6315d
iamtinkal <116683248+iamtinkal@users.noreply.github.com>
2024-08-29 17:43:15 +0000

    Projcec 4 done

--- Project 4: Guess The Number/script.js
+++ Project 4: Guess The Number/script.js
@@ -1,6 +1,6 @@
-// const random_number = parseInt(Math.random() * 100) + 1
+let random_number = parseInt(Math.random() * 100 + 1)
 
-random_number = 70
+// random_number = 70
 // const input = document.querySelector('#guessField').value
 
 record_guess = []
@@ -21,41 +21,51 @@ if (play_game) {
 
     sumbit.addEventListener('click', function (e) {
         e.preventDefault();
-        let guess = parseInt(input.value);
-        // input = parseInt(document.querySelector('#guessField').value)
 
-
-
-        if (guess === random_number) {
-            alert("Congratulation! You Won!")
-            endGame()
-
-        }
-        else if (guess < random_number) {
-            document.querySelector(".lowOrHi").innerHTML = "Guess is too Low!"
+        if (input.value <= 0 || input.value > 100 || isNaN(input.value)) {
+            alert("Enter Valid Number")
         }
         else {
-            document.querySelector(".lowOrHi").innerHTML = "Guess is too high!"
-        }
-        // console.log(input);
-        if (play_game) {
-            record_guess.push(guess)
-            document.querySelector('.guesses').innerHTML = record_guess
+            let guess = parseInt(input.value);
+            // input = parseInt(document.querySelector('#guessField').value)
 
-            input.value = ''
 
-            remaining_guess = document.querySelector('.lastResult')
-            remaining_guess.innerHTML -= 1
-            pending_guess -= 1
 
-            if (pending_guess === 0) {
-                alert("You Lost!")
+            if (guess === random_number) {
+                alert("Congratulation! You Won!")
+                endGame()
+
             }
+            else if (guess < random_number) {
+                document.querySelector(".lowOrHi").innerHTML = "Guess is too Low!"
+            }
+            else {
+                document.querySelector(".lowOrHi").innerHTML = "Guess is too high!"
+            }
+            // console.log(input);
+            if (play_game) {
+                record_guess.push(guess)
+                document.querySelector('.guesses').innerHTML = record_guess
+
+                input.value = ''
+
+                remaining_guess = document.querySelector('.lastResult')
+                remaining_guess.innerHTML -= 1
+                pending_guess -= 1
+
+                if (pending_guess === 0) {
+                    alert("You Lost!")
+                    alert(`Correct Number is:${random_number}`)
+                    endGame()
+                }
+            }
+
+
         }
-
-
     })
 }
+
+
 
 
 
@@ -68,7 +78,7 @@ function endGame() {
     p.classList.add('button');
     p.innerHTML = `<h2 id="newGame">Start new Game</h2>`;
     startOver.appendChild(p);
-    play_game = false;
+    document.querySelector(".lowOrHi").innerHTML = ''
 
     new_game();
 }
@@ -77,7 +87,7 @@ function endGame() {
 function new_game() {
     const newGameButton = document.querySelector('#newGame');
     newGameButton.addEventListener('click', function (e) {
-        random_number = 70;
+        random_number = parseInt(Math.random() * 100 + 1);
         document.querySelector('.guesses').innerHTML = '';
 
         input.value = ''
@@ -85,6 +95,9 @@ function new_game() {
         input.removeAttribute('disabled');
         startOver.removeChild(p);
         record_guess = []
+        pending_guess = 10
+        document.querySelector(".lowOrHi").innerHTML = ''
         play_game = true;
+
     });
 }
